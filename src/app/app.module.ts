3d05acb741c7fdcf57362482ec6e6f7850b38fd1
Federico Peralta <federico.peralta91@gmail.com>
2025-04-02 15:53:45 +0200

    pagina chi sono basi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ChiSiamoComponent } from './pages/chi-siamo/chi-siamo.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { CompetenzeComponent } from './pages/competenze/competenze.component';
 import { ContattiComponent } from './pages/contatti/contatti.component';
@@ -16,21 +18,15 @@ import { ChiSonoComponent } from './pages/chi-sono/chi-sono.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    ChiSiamoComponent,
     PortfolioComponent,
     CompetenzeComponent,
     ContattiComponent,
     NavbarComponent,
     FooterComponent,
-    ChiSonoComponent
+    ChiSonoComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
