be2273be8834a493db716bf11dff7995953dfea9
Federico Peralta <federico.peralta91@gmail.com>
2025-04-04 15:40:16 +0200

    1 approccio tutto ok funzionante

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isHomePage = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Se serve per sicurezza (non strettamente obbligatorio se sei già nella home)
+    this.isHomePage = this.router.url === '/';
+  }
 }
